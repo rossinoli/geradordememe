87ea4b4914029d52d82bba2af25d3115b59157a3
thaina rossinoli <rossinoli.2001@gmail.com>
2023-04-01 15:36:12 -0300

    geradordememe

---
+++ script.js
@@ -0,0 +1,14 @@
+function update_text(){
+    var user_text = document.getElementById("user_text");
+    var meme_text = document.getElementById("meme_text");
+    meme_text.innerHTML = user_text.value;
+}
+
+function update_image(){
+    var img = document.querySelector('img');
+    var file = document.querySelector('input[type=file]').files[0];
+    img.src = window.URL.createObjectURL(file);
+}
+
+
+
